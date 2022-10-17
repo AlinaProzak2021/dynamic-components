7a873f2f07d1ba171f609c9ec6cc668e00ec8581
AlinaProzak2021 <foxic210@gmail.com>
2022-10-17 12:19:13 +0300

    create project and added resources

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <homePage />
+</template>
+
+<script>
+import homePage from "./components/HomePage.vue";
+
+export default {
+  name: "App",
+  components: {
+    homePage,
+  },
+};
+</script>
+
+<style>
+:root {
+  --lightTheme: #fffafa;
+  --darkTheme: #0b0324;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  background-color: var( --lightTheme);
+  width: 100%;
+  height: 100%;
+}
+</style>
